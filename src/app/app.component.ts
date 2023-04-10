b1e3c2895bd7da5dcd994bc7d15833f4803fcb5f
labibi.lg <labibi.lg@gmail.com>
2023-04-10 17:21:53 +0800

    feat: Angular基本架構

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import SeatchartJS, { Options } from 'seatchart';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'labibi-angular-template';
+
+  // constructor(
+  //   private SeatchartJS:SeatchartJS
+  // ) {
+
+  // }
+
+}
+
