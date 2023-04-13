9b72cda8fdf4044e409c78311df97e8b22b1ce8e
labibi.lg <labibi.lg@gmail.com>
2023-04-13 17:46:26 +0800

    更新測試

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import SeatchartJS, { Options } from 'seatchart';
 
-interface SeatchartProps {
-  options: Options;
-}
+// interface SeatchartProps {
+//   options: Options;
+// }
 
 @Component({
   selector: 'app-root',
@@ -12,16 +12,64 @@ interface SeatchartProps {
 })
 export class AppComponent {
   title = 'labibi-angular-template';
-
-  constructor(
-    private seatchartJS:SeatchartJS
-  ) {
-
-  }
+  @ViewChild('elementRef', { static: true }) elementRef!: ElementRef<HTMLDivElement>;
+  options: Options = {
+    map: {
+      rows: 10,
+      columns: 10,
+      seatTypes: {
+        default: {
+          label: 'Economy',
+          cssClass: 'economy',
+          price: 15,
+        },
+        first: {
+          label: 'First Class',
+          cssClass: 'first-class',
+          price: 25,
+          seatRows: [0, 1, 2],
+        },
+        reduced: {
+          label: 'Reduced',
+          cssClass: 'reduced',
+          price: 10,
+          seatRows: [7, 8, 9],
+        },
+      },
+      disabledSeats: [
+        { row: 0, col: 0 },
+        { row: 0, col: 9 },
+      ],
+      reservedSeats: [
+        { row: 0, col: 3 },
+        { row: 0, col: 4 },
+      ],
+      selectedSeats: [{ row: 0, col: 5 }, { row: 0, col: 6 }],
+      rowSpacers: [3, 7],
+      columnSpacers: [5],
+    },
+  };
+  private seatchartRef?: SeatchartJS;
+  constructor() { }
 
   ngOnInit(): void {
-    console.log(this.seatchartJS);
+    if (this.elementRef && !this.seatchartRef) {
+      this.seatchartRef = new SeatchartJS(this.elementRef.nativeElement, this.options);
+      this.seatchartRef.clearCart();
+    }
+  }
+  ngOnDestroy(): void {
+    if (this.seatchartRef) {
+      // this.seatchartRef.destroy();
+      this.seatchartRef = undefined;
+    }
   }
 
+  tt(): void{
+    this.seatchartRef!.addEventListener('cartchange',function(e){
+      console.log(e);
+    })
+  }
 }
+
 
